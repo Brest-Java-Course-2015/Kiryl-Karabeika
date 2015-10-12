c1a7e3c27346e890e68266f4d058352a8866df49
kirylkarabeika <kiryl.k.14@gmail.com>
2015-10-12 20:52:48 +0300

    dao interface is separated from implementation

--- app-dao/src/main/java/com/epam/brest/course2015/dao/UserDaoImpl.java
+++ app-dao/src/main/java/com/epam/brest/course2015/dao/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package com.epam.brest.course2015.dao;
 import com.epam.brest.course2015.domain.User;
+import com.epam.brest.course2015.dao.UserDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
